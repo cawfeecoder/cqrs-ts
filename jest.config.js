0de053de3eb78c9d93ec06fed324cca1066b254e
cawfeecoder <nick@frushco.de>
2022-11-12 20:56:08 -0500

    test first test

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  testEnvironment: "node",
+  moduleNameMapper: pathsToModuleNameMapper(
+    compilerOptions.paths /*, { prefix: '<rootDir>/' } */
+  ),
+};
---
+++ src/context/prescription/domain/entity/aggregate.spec.ts
@@ -0,0 +1,27 @@
+import { PrescriptionAggregate } from "./aggregate";
+import { CreatePrescriptionCommand } from "./command";
+import { PrescriptionCreatedEvent } from "./event";
+
+describe("PrescriptionAggregate", () => {
+  it("should emit a PrescriptionCreated event when receives a CreatePrescriptionCommand", async () => {
+    // Arange
+    let command = new CreatePrescriptionCommand({
+      medicationId: "test",
+      patientId: "test",
+      address: "test",
+    });
+
+    let aggregate = new PrescriptionAggregate();
+
+    // Act
+    let event = await aggregate.handle(command, {});
+
+    //Assert
+    expect(event.isOk());
+    let unwrapped = event.unwrap() as PrescriptionCreatedEvent;
+    expect(unwrapped.eventType()).toEqual("PrescriptionCreated");
+    expect(unwrapped.address).toEqual("test");
+    expect(unwrapped.medicationId).toEqual("test");
+    expect(unwrapped.patientId).toEqual("test");
+  });
+});
